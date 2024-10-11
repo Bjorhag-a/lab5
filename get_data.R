e49eb9fa7fdca1e84c567119635e4a95ddf29223
jakli758 <jakli758@student.liu.se>
2024-10-11 21:15:08 +0200

    deleted unused lines

--- get_data.R
+++ get_data.R
@@ -83,37 +83,6 @@ values
 
 
 
-library(shiny)
 
-
-ui <- fluidPage(
-  
-  page_sidebar(..., sidebar = NULL, title = NULL, fillable = TRUE, fillable_mobile = FALSE, 
-               theme = bs_theme(), window_title = NA, lang = NULL)
-  
-  textInput(inputId = "curr", "Currency", "SEK"),
-  numericInput(inputId = "p", 
-               label = "Principal", 
-               value = 100, min = 0),
-  numericInput(inputId = "r", 
-               label = "Interest Rate %", 
-               value = 1, min = 0),
-  numericInput(inputId = "t", 
-               label = "Holding Period", 
-               value = 1, min = 0),
-  textOutput(outputId = "amt")
-)
-
-server <- function(input, output) {
-  
-  int <- reactive(input$pinput$rinput$t/100)
-  output$amt <- renderText({
-    paste("Amount at Maturity (Rounded to 2 decimal places):", 
-          input$curr, as.character(round(input$p + int(),  2)))
-  })
-  
-}
-
-shinyApp(ui, server)
 
 
