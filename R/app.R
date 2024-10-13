8f2b938047ae692fc252e8257e3f1a700261c4ec
jakli758 <jakli758@student.liu.se>
2024-10-13 15:36:11 +0200

    reverted change in app, as runGitHub does not work anymore

--- R/app.R
+++ R/app.R
@@ -6,6 +6,9 @@
 #' then creates a bar plot divided by gender.  
 #' 
 #' 
+#' @param ui shiny UI
+#' @param server shiny server
+#' 
 #' @import shiny
 #' @import ggplot2
 #' @import magrittr
@@ -13,73 +16,69 @@
 #' @export
 #' 
 
-app <- function(){
-  
-  ui <- fluidPage(
-    titlePanel("Kolada"),
-    sidebarLayout(
-      sidebarPanel(
-        selectInput("m", "Select municipality", choices = NULL),
-        selectInput("kpi", "Select KPI", choices = NULL),
-        selectInput("year", "Select year", choices = c(
-          "2010",
-          "2011",
-          "2012",
-          "2013",
-          "2014",
-          "2015",
-          "2015",
-          "2016",
-          "2017",
-          "2018", 
-          "2019"))
-      ),
-      mainPanel(
-        plotOutput("plot")
-      )
-    )
-  )
-  
-  server <- function(input, output, session){
-    # get kpis for later reference for 
-    kpi_group <- get_kpi_group("GKPI127")
-    kpi_members <- rbind.data.frame(kpi_group$members)
-    
-    observe({
-      updateSelectInput(session, "m", choices = get_municipalities())
-      updateSelectInput(session, "kpi", choices = kpi_members$member_id)
-    })
-    
-    d <- reactive({
-      get_data(input$kpi, input$m, input$year)}
-    ) %>% 
-      # cache the data to avoid repetitive API
-      bindCache(input$kpi, input$m, input$year)
-    
-    
-    
-    output$plot <- renderPlot({
-      validate(
-        need(is.data.frame(d()), "The inputs have no data")
-      )
-      
-      ggplot(d(), aes(x = gender, y = value, fill=gender)) + 
-        geom_bar(stat = "identity") +
-        
-        #print name of KPI --> Select one KPI group 
-        labs(title = paste("Data for", kpi_members$member_title[kpi_members$member_id==input$kpi]),
-             x = "Gender", y = "Values") +
-        theme_bw()
-    }) %>% 
-      bindCache(d())
-  }
+app <- function(ui, server){
   shinyApp(ui, server)
 }
 
 
+ui <- fluidPage(
+  titlePanel("Kolada"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("m", "Select municipality", choices = NULL),
+      selectInput("kpi", "Select KPI", choices = NULL),
+      selectInput("year", "Select year", choices = c(
+        "2010",
+        "2011",
+        "2012",
+        "2013",
+        "2014",
+        "2015",
+        "2015",
+        "2016",
+        "2017",
+        "2018", 
+        "2019"))
+    ),
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
 
-
-
+server <- function(input, output, session){
+  # get kpis for later reference for 
+  kpi_group <- get_kpi_group("GKPI127")
+  kpi_members <- rbind.data.frame(kpi_group$members)
+  
+  observe({
+    updateSelectInput(session, "m", choices = get_municipalities())
+    updateSelectInput(session, "kpi", choices = kpi_members$member_id)
+  })
+  
+  d <- reactive({
+    get_data(input$kpi, input$m, input$year)}
+  ) %>% 
+    # cache the data to avoid repetitive API
+    bindCache(input$kpi, input$m, input$year)
+  
+  
+  
+  output$plot <- renderPlot({
+    validate(
+      need(is.data.frame(d()), "The inputs have no data")
+    )
+    
+    ggplot(d(), aes(x = gender, y = value, fill=gender)) + 
+      geom_bar(stat = "identity") +
+      
+      #print name of KPI --> Select one KPI group 
+      labs(title = paste("Data for", kpi_members$member_title[kpi_members$member_id==input$kpi]),
+           x = "Gender", y = "Values") +
+      theme_bw()
+  }) %>% 
+    bindCache(d())
+}
 
 
 
